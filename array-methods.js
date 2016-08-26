153521889cc9a843cd22d24f900ad575210b4ade
Alicia Miceli <aliciamiceli@Alicias-MacBook-Pro.local>
2016-08-25 21:09:16 -0500

    finished

--- array-methods.js
+++ array-methods.js
@@ -12,18 +12,36 @@ planets.forEach( function outputPlanets(){
 
 
 // Use the map method to create a new array where the first letter of each planet is capitalized
+var caps = planets.map(function(newPlanets) {
+  return newPlanets.charAt(0).toUpperCase() + newPlanets.slice(1);
+});
 
-function capitalize(caps) {
-  return caps.charAt(0);
-}
-
-planetsCapitalized = planets.map(capitalize);
-console.log(planetsCapitalized);
-
+console.log(caps);
 
 
 // Use the filter method to create a new array that contains planets with the letter 'e'
 
+function filterIt(myPlanets) {
+  if (myPlanets.indexOf('e') != -1) {
+    return myPlanets;
+  }
+}
+
+var filteredPlanets = planets.filter(filterIt);
+console.log(filteredPlanets);
+
+
+
 // Use the reduce method to create a sentence from the words in the following array
 
 var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
+
+function sentence(previousValue, currentValue) {
+  return previousValue + currentValue;
+}
+
+var newerWords = words.reduce(sentence);
+console.log(newerWords);
+
+var newWords = words.reduce( (prev, curr) => prev + curr );
+console.log(newWords);
